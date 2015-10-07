77ee03f530d23b1b028c9335a5497cf775be77b2
odedlingualy <oded@lingua.ly>
2015-10-07 09:56:26 +0300

    Support automated builds
    Add automatic retrieval of auth token and storage url from Object Storage server, and use them in test calls.

--- ge_object_storage/smoketest.sh
+++ ge_object_storage/smoketest.sh
@@ -5,29 +5,44 @@ PORT=$2
 
 URL="$HOST:$PORT"
 
-echo Get token:
-curl -v -H 'X-Storage-User: test:tester' -H 'X-Storage-Pass: testing' $URL
+echo -e '\n'
+echo == Get Storage url and token: ==
 
-echo Create container:
+STORAGE_URL=$(curl -i -H 'X-Storage-User: test:tester' -H 'X-Storage-Pass: testing' $URL/auth/v1.0 | grep X-Storage-Url: | awk {'print $2'})
+TOKEN=$(curl -i -H 'X-Storage-User: test:tester' -H 'X-Storage-Pass: testing' $URL/auth/v1.0 | grep X-Auth-Token: | awk {'print $2'})
 
-curl -X PUT -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+# clean new line chars
+STORAGE_URL=$(echo $STORAGE_URL|tr -d '\r')
+STORAGE_URL=$(echo $STORAGE_URL|tr -d '\n')
+TOKEN=$(echo $TOKEN|tr -d '\r')
+TOKEN=$(echo $TOKEN|tr -d '\n')
 
-echo List containers:
+echo -e '\n'
+echo == Create container: ==
 
-curl -X GET -i -H "X-Auth-Token: $TOKEN" $URL
+curl -X PUT -i -H "X-Auth-Token: $TOKEN" $STORAGE_URL/testcontainer
 
-echo Contents of container:
+echo -e '\n'
+echo == List containers: ==
 
-curl -X GET -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+curl -X GET -i -H "X-Auth-Token: $TOKEN" $STORAGE_URL
 
-echo Metadata of container:
+echo -e '\n'
+echo == Contents of container: ==
 
-curl -X HEAD -i -H "X-Auth-Token: $TOKEN" $URL/testcontainer
+curl -X GET -i -H "X-Auth-Token: $TOKEN" $STORAGE_URL/testcontainer
 
-echo Put object:
+echo -e '\n'
+echo == Metadata of container: ==
 
-curl -X PUT -i -H "X-Auth-Token: $TOKEN" -T demo.sh $URL/testcontainer/testobject
+curl -X HEAD -i -H "X-Auth-Token: $TOKEN" $STORAGE_URL/testcontainer
 
-echo Retrieve object:
+echo -e '\n'
+echo == Put object: ==
 
-curl -X GET -i -H "X-Auth-Token: $TOKEN" -o retrieved_demo.sh $URL/testcontainer/testobject
+curl -X PUT -i -H "X-Auth-Token: $TOKEN" -T demo.sh $STORAGE_URL/testcontainer/testobject
+
+echo -e '\n'
+echo == Retrieve object: ==
+
+curl -X GET -i -H "X-Auth-Token: $TOKEN" -o retrieved_demo.sh $STORAGE_URL/testcontainer/testobject
