168e550955d9f4d3faa017968ac1e4d1eccffd9e
abbas ahmad <abbas.ahmad@eglobalmark.com>
2015-11-26 16:04:51 +0100

    New Script for automated testing

--- interrogators/OrionCB/OrionCB_EGM_Tests.sh
+++ interrogators/OrionCB/OrionCB_EGM_Tests.sh
@@ -1,18 +1,30 @@
-SLEEPTIME=30
+#!/bin/bash
+WAITFOR=30
+echo "WAITFOR time defined = $WAITFOR"
 
-mkdir OrionCBTesting
-cd OrionCBTesting
+echo "Creating test Directory"
+mkdir ./orioncbtesting
+cd orioncbtesting
+
+echo "########## Actualy going to run in $(pwd) ##########"
 echo "Getting the docker compose file from https://raw.githubusercontent.com/telefonicaid/fiware-orion/develop/docker-compose.yml"
 wget https://raw.githubusercontent.com/telefonicaid/fiware-orion/develop/docker/docker-compose.yml
 docker-compose up -d
-sleep $SLEEPTIME
+echo "Going to sleep : Give time for docker instances to spawn"
+sleep $WAITFOR
 
+echo "Downloading the EGM functional test"
 wget https://bitbucket.org/eglobalmark/egm_testexecorioncb/raw/b69758c3aa5f386e8e30d783d92c93a8e6783a49/testExecOrionCB.jar
+
+echo "Executing the EGM functional test on 127.0.0.1:1026 (Orion instance) and reporting tests result to : http://207.249.127.73:7070/report"
 java -jar testExecOrionCB.jar -p 1026 -ip http://127.0.0.1 -o http://207.249.127.73:7070/report -t 2 -header Fiware-Service#egmtestexectests@Fiware-ServicePath#/egmtestexectests
 
-docker rm -f OrionCBTesting_mongo_1
-docker rm -f OrionCBTesting_orion_1
+echo "removing Docker instances created..."
+docker rm -f orioncbtesting_mongo_1
+docker rm -f orioncbtesting_orion_1
 
+echo "Deleting created directory"
 cd ..
-rm -r OrionCBTesting
+rm -r ./orioncbtesting
 
+echo "Bye bye"
