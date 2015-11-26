9a9f2a47bfb070b283c96a7010b271ab35ff81ac
abbas ahmad <abbas.ahmad@eglobalmark.com>
2015-11-26 16:30:48 +0100

    Add Status Code Echo to Log and Jenkins Global result return code based on tests return code

--- interrogators/OrionCB/OrionCB_EGM_Tests.sh
+++ interrogators/OrionCB/OrionCB_EGM_Tests.sh
@@ -19,6 +19,9 @@ wget https://bitbucket.org/eglobalmark/egm_testexecorioncb/raw/b69758c3aa5f386e8
 echo "Executing the EGM functional test on 127.0.0.1:1026 (Orion instance) and reporting tests result to : http://207.249.127.73:7070/report"
 java -jar testExecOrionCB.jar -p 1026 -ip http://127.0.0.1 -o http://207.249.127.73:7070/report -t 2 -header Fiware-Service#egmtestexectests@Fiware-ServicePath#/egmtestexectests
 
+STATUS="${?}" ;
+echo "Global Exit Code of Tests : ${STATUS}"
+
 echo "removing Docker instances created..."
 docker rm -f orioncbtesting_mongo_1
 docker rm -f orioncbtesting_orion_1
@@ -27,4 +30,5 @@ echo "Deleting created directory"
 cd ..
 rm -r ./orioncbtesting
 
-echo "Bye bye"
+echo "Bye bye!! Exit with status code ${STATUS}"
+exit $STATUS
