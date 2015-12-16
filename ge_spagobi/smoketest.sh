209e11cb69726ad47f2e005eea66ef63396993b9
julia-nf <j.nieddu-fitremann@beaconinside.com>
2015-12-16 11:31:51 +0100

    begin smoketest with building image and running containers

--- ge_spagobi/smoketest.sh
+++ ge_spagobi/smoketest.sh
@@ -2,9 +2,52 @@
 
 set -e
 
+# Remove old containers
+echo $(docker rm -f $(docker ps -a -q))
+
+# Build images and run containers
+echo
+echo Build images
+
+echo $(docker build -t spago_image .) &
+wait
+echo
+echo Building SpagoBI image
+
+
+echo $(docker run --name mysqldb_container -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_DATABASE=mysql_db -e MYSQL_ROOT_PASSWORD=root -d mysql) &
+wait
+echo MySQL container run
+
+echo
+
+echo $(mysql.server start)
+
+echo
+echo SpagoBI image built
+
+sleep 2 && echo $(docker run --name spago_container -e DB_PORT_3306_TCP_ADDR=localhost --link mysqldb_container -P spago_image) &
+wait
+echo
+echo Running SpagoBI container
+
+echo
+echo SpagoBI container run
+
+cd external_db 
+echo
+echo $(docker-compose up) &
+wait
+echo
+echo Running docker-compose
+
+echo MySQL datasource run
+
+
 # Get container id
 container=($(docker ps | awk '{ print $1 }'))
 container_id=${container[1]}
+
 
 # Get IP depending on the OS : IP of the VM on Mac OS, IP of the container on others
 if [[ `uname` == 'Darwin'* ]]; then
@@ -23,10 +66,10 @@ address=$(echo $IP:$port)
 echo "Docker-machine test"
 # make sure the virtual machine is on
 if [[ -z $IP ]]; then
-	echo "Docker container not running\n"
+	echo "Docker machine not running\n"
 	exit 1
 else
-	echo "Docker container found at IP $IP\n"
+	echo "Docker machine found at IP $IP\n"
 fi
 
 echo "Port test"
