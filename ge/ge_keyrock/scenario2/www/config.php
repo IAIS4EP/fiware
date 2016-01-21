e72820d3cd79a7aade52ee3f6ba8f883cae3964f
naina <naina@tablegrabber.in>
2016-01-21 14:59:30 +0530

    Scenario2 files added

---
+++ ge/ge_keyrock/scenario2/www/config.php
@@ -0,0 +1,32 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| FIWARE APP ID
+|--------------------------------------------------------------------------
+|
+| FIWARE App Id is used for fiware plugins
+|
+|
+|
+*/
+
+$config['fiwareClientId'] = '3f32e62192ed4d5cad07b5457ff1af81';
+/*
+|--------------------------------------------------------------------------
+| Fiware Secret  
+|--------------------------------------------------------------------------
+|
+| Login with Fiware ?
+|
+|	
+|
+*/
+$config['fiwareSecret'] = '4f0ccd9464124874bc8aa3b68432849f';
+
+/*--------------------- cache time limits  ------------------------*/
+
+$config['cache_main'] = 3600*24*30;
+
+$config['cache_for_day'] = 3600*24;
+
