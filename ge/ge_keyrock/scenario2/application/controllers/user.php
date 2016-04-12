4b794fbd40c419e9f6685599b5e055175785fb95
gitnaina <naina@tablegrabber.in>
2016-04-12 11:27:24 +0530

    Update user.php

--- ge/ge_keyrock/scenario2/application/controllers/user.php
+++ ge/ge_keyrock/scenario2/application/controllers/user.php
@@ -2,21 +2,7 @@
 
 class User extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	
 	public function index()
 	{
 		if($this->session->userdata('access_token')){
@@ -41,5 +27,5 @@ class User extends CI_Controller {
 	}
 }
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file user.php */
+/* Location: ./application/controllers/user.php */
