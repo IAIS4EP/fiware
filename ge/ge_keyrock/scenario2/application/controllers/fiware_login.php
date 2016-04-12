55df3de35b03d07a1b24c760c273ab7884c58944
gitnaina <naina@tablegrabber.in>
2016-04-12 11:26:00 +0530

    Update fiware_login.php

--- ge/ge_keyrock/scenario2/application/controllers/fiware_login.php
+++ ge/ge_keyrock/scenario2/application/controllers/fiware_login.php
@@ -2,27 +2,13 @@
 
 class Fiware_Login extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -  
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in 
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
-	 */
+	
 	 
 	function __construct()
 	{
 		parent::__construct();
 		$this->load->model('model_user','',true);
-    }
+    	}
 	
 	public function index()
 	{
@@ -37,7 +23,7 @@ class Fiware_Login extends CI_Controller {
 	
 		session_start();
 		
-		//facebook application
+		//fiware application
 		$fiwareClientId = $this->config->item( 'fiwareClientId' );
 		$fiwareSecret = $this->config->item( 'fiwareSecret' );
 
@@ -45,41 +31,35 @@ class Fiware_Login extends CI_Controller {
 		$fwconfig['secret']     = $fiwareSecret;
 		$fwconfig['baseurl']    = base_url();
 
-		//facebook user uid
+		//fiware user uid
 		$user            =   null; 
 		$code = $this->input->get('code');
 		
 		/*-----------------
-			IF FB CODE
+			IF FIWARE CODE
 		-----------------*/		
 				
 	   if(empty($code)) {
 
-			//$_SESSION['state'] = md5(uniqid(rand(), TRUE)); // CSRF protection
+			
 	   		$rand = md5(uniqid(rand(), TRUE));
 			$this->session->set_userdata('state',$rand);
 			$dialog_url = "https://account.lab.fiware.org/oauth2/authorize?response_type=code&client_id=" 
 			. $fwconfig['appid'] . "&redirect_uri=" . urlencode(base_url().'index.php/fiware_login/fwlogin') . "&state="
 			. $rand;
-			//redirect($dialog_url );
+			
 
 			 echo("<script> top.location.href='" . $dialog_url . "'</script>");
 	   }
 		
 		/*--------------------------------
-			GET ACCESS TOKEN FOR FB USER
+			GET ACCESS TOKEN FOR FIWARE USER
 		---------------------------------*/
 		
 		$sessionState=$this->session->userdata('state');
 		$requestState=$this->input->get('state');
 		if($sessionState && ($sessionState === $requestState)){
-    		//$token_url = "https://account.lab.fiware.org/oauth2/token?". "client_id=" .$fwconfig['appid']."&redirect_uri=".urlencode(BASEURL.'userTask/login/fwlogin'). "&client_secret=" . $fwconfig['secret']. "&code=" . $code. "&grant_type=authorization_code";
-			//echo $token_url;
     		
-			//$response = file_get_contents($token_url); 
-     		//$params = null;
-     		//parse_str($response, $params);
-			
 			$url="https://account.lab.fiware.org/oauth2/token";
 			$params = array();
 			$params["client_id"] = $fwconfig['appid'];
@@ -116,20 +96,17 @@ class Fiware_Login extends CI_Controller {
 			}
 			
 			$data=json_decode($data,true);
-			//echo $data['access_token'];
-		//print_r($data); die;
-
-			
 			
      		$this->session->set_userdata('access_token',$data['access_token']); 
      		/*----------* SAVE FIWARE USER DATA *----------*/
  
 			$result = $this->model_user->saveFiwareUser($data['access_token']);
-			//print_r($result);die;
+			
 			if($result['status']){
  				redirect(base_url().'index.php/user');			
 			} else{
-				$this->messages->setMessage($result['message'],'alert alert-warning');
+			//	$this->messages->setMessage($result['message'],'alert alert-warning');
+			echo $result['message'];
 			}
    		}
    		else {
@@ -144,5 +121,5 @@ class Fiware_Login extends CI_Controller {
 
 
 
-/* End of file welcome.php */
-/* Location: ./application/controllers/welcome.php */
+/* End of file fiware_login.php */
+/* Location: ./application/controllers/fiware_login.php */
